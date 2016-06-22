5f3688e644eab8e9408ceb93cb662f326f3603c7
Tushar Rakheja <tusharrakheja@gmail.com>
2016-06-22 09:09:06 +0530

    Okay work now.

--- Files/Header Files/AbstractSet.h
+++ Files/Header Files/AbstractSet.h
@@ -12,8 +12,13 @@ public:
 	AbstractSet() : Elem(ABSTRACT_SET) { }			   // Uninteresting default constructor.
 	AbstractSet(string &);					   // Construct with a string representing the criteria.
 	AbstractSet * cartesian_product(AbstractSet &);            // Returns the cartesian product of this set and the other set.
-	Elem * deep_copy()
-	{ return new AbstractSet((string)"{ elem | " + criteria + (string)" }"); } // Return a new set with the same criteria.
+	Elem * deep_copy()					   // Return a new set with the same criteria.
+	{ 
+		string crit_copy = "{ elem | ";
+		crit_copy += criteria;
+		crit_copy += " }";
+		return new AbstractSet(crit_copy); 
+	} 
 	AbstractSet * exclusion(AbstractSet &);			   // Returns a set containing elements of this, minus those of the argument.
 	bool has(Elem &);					   // Looks for an element in the set.
 	AbstractSet * intersection(AbstractSet &);                 // Intersection with a second set.
